75dcbaeb2ca7e7c79a0a5f89ba51641a0c4a1f98
Filipe Medeiros <filipesilvamedeiros@gmail.com>
2022-03-28 12:40:28 +0100

    emails save

---
+++ pages/api/saveEmail.ts
@@ -0,0 +1,26 @@
+import mysql from 'mysql2/promise'
+import { NextApiHandler } from 'next'
+
+const emailRegex =
+  /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
+
+const saveEmail: NextApiHandler = async (req, res) => {
+  if (req.method !== 'POST') {
+    res.status(401).end()
+    return
+  }
+
+  const email: string = req.body.email ?? `Undefined email ${new Date()}`
+
+  if (!emailRegex.test(email)) {
+    res.status(400).end()
+    return
+  }
+
+  const connection = await mysql.createConnection(process.env.PSCALE_URL!)
+  await connection.query(`INSERT INTO Emails VALUES ("${email}")`)
+
+  res.status(200).end()
+}
+
+export default saveEmail
--- pages/subscribe.tsx
+++ pages/subscribe.tsx
@@ -1,6 +1,6 @@
 import { NextPage } from 'next'
 import Image from 'next/image'
-import { useEffect } from 'react'
+import { useEffect, useRef } from 'react'
 
 import { useOffCanvasNavContext } from '../hooks/useOffCanvasNav'
 import subscribePicture from '../public/pictures/subscribe.jpg'
@@ -8,6 +8,8 @@ import subscribePicture from '../public/pictures/subscribe.jpg'
 const Subscribe: NextPage = () => {
   const { setOpen } = useOffCanvasNavContext()
   useEffect(() => setOpen(false), [setOpen])
+
+  const emailRef = useRef<HTMLInputElement>(null)
 
   return (
     <>
@@ -19,8 +21,19 @@ const Subscribe: NextPage = () => {
             everything DITCH DAYS.
           </p>
         </div>
-        <form className="contents">
+        <form
+          className="contents"
+          onSubmit={ev => {
+            ev.preventDefault()
+            fetch('/api/saveEmail', {
+              method: 'POST',
+              headers: [['Content-Type', 'application/json']],
+              body: JSON.stringify({ email: emailRef.current?.value }),
+            })
+          }}
+        >
           <input
+            ref={emailRef}
             className="self-start bg-transparent border-2 border-white px-5 py-2 leading-none placeholder:font-extralight"
             placeholder="Email address"
           />
