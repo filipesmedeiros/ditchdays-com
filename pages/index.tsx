0ce170d899670238ee3159191b1cf58f481fcc8c
Filipe Medeiros <filipesilvamedeiros@gmail.com>
2022-03-31 15:23:43 +0100

    fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,8 +11,8 @@ const Home: NextPage = () => {
   useEffect(() => setOpen(false), [setOpen])
 
   return (
-    <div className="flex flex-col justify-center h-full relative">
-      <h1 className="text-center text-transparent text-10xl py-2 font-bold font-lc leading-[0.8] bg-clip-text bg-[url('/pictures/subscribe.jpg')] bg-[position:-90px_-46px] bg-[length:520px]">
+    <div className="flex flex-col justify-center h-full relative items-center">
+      <h1 className="text-center text-transparent text-10xl py-2 max-w-fit font-bold font-lc leading-[0.8] bg-clip-text bg-[url('/pictures/subscribe.jpg')] bg-[position:-146px_-46px] bg-[length:520px]">
         DITCH
         <br />
         DAYS
