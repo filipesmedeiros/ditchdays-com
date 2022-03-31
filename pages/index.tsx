dae61cc5213621dcdb6aba0140a5e32b0f698b12
Filipe Medeiros <filipesilvamedeiros@gmail.com>
2022-03-31 13:54:44 +0100

    pic

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,7 +12,7 @@ const Home: NextPage = () => {
 
   return (
     <div className="flex flex-col justify-center h-full relative">
-      <h1 className="text-center text-transparent text-10xl py-2 font-bold font-lc leading-[0.8] bg-clip-text bg-[url('/pictures/subscribe.jpg')] bg-[position:-90px-46px] bg-[length:520px]">
+      <h1 className="text-center text-transparent text-10xl py-2 font-bold font-lc leading-[0.8] bg-clip-text bg-[url('/pictures/subscribe.jpg')] bg-[position:-90px_-46px] bg-[length:520px]">
         DITCH
         <br />
         DAYS
