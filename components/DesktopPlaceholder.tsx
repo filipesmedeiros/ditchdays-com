405a4f067ae82f51ceeae1e0549d5d9c586a2c55
Filipe Medeiros <filipesilvamedeiros@gmail.com>
2022-04-01 13:37:44 +0200

    fix

--- components/DesktopPlaceholder.tsx
+++ components/DesktopPlaceholder.tsx
@@ -2,7 +2,7 @@ import { FC } from 'react'
 
 const DesktopPlaceholder: FC = () => (
   <main className="flex-col gap-6 justify-center items-center w-screen h-screen px-6 container py-10 m-auto overflow-hidden hidden md:flex sm:portrait:hidden">
-    <h1 className="text-center text-9xl font-extrabold leading-[0.8]">
+    <h1 className="text-center text-9xl font-bold font-lc leading-[0.8]">
       DITCH DAYS
     </h1>
     <h2 className="text-center text-6xl font-extralight font-hc text-orange">
